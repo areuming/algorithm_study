00fbec4c2ea3922cef7560bc276ddb4306611d8c
areuming <djarys92@gmail.com>
2024-11-13 11:25:57 +0900

    study: 기본수학문제_곱셈

---
+++ src/Main_10998.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main_10998 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+
+        int an = n1 * n2;
+
+        System.out.println(an);
+    }
+}
